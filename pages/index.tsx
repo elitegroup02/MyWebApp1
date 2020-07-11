bf420ca908f3458a92169dde120602bc8eff1838
Juan Pablo Rodriguez Acevedo <juanpablo@magoya.com>
2020-07-11 19:23:39 -0300

    add three.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import {Canvas} from '../components/3d/index';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+
+    Canvas();
+
+    return (
+        <Layout title="Home | Next.js + TypeScript Example">
+            <h1>Hello Next.js 👋</h1>
+            <p>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+            </p>
+        </Layout>
+    )
+
+}
 
 export default IndexPage
