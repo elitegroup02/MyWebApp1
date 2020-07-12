d5b0b6be0fda2ebc5c9924b055e118580fa16cde
Juan Pablo Rodriguez Acevedo <juanpablo@magoya.com>
2020-07-12 13:07:33 -0300

    Fix client-side-only reqs

--- components/3d/index.tsx
+++ components/3d/index.tsx
@@ -1,29 +1,39 @@
 import * as THREE from 'three';
+import React, {useState, useEffect} from 'react';
+
 
 export const Canvas = () => {
-    window = window || {}
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const [clientSide, setClientSide] = useState(String)
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    useEffect(() => {
+        setClientSide('true');
+    })
 
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    if(!!clientSide){
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    camera.position.z = 5;
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
 
-    var animate = () => {
-        requestAnimationFrame( animate );
+        var geometry = new THREE.BoxGeometry();
+        var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        camera.position.z = 5;
 
-        renderer.render( scene, camera );
-    };
+        var animate = () => {
+            requestAnimationFrame(animate);
 
-    animate();
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render(scene,camera);
+        };
+
+        animate();
+    }
+
 }
